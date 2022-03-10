a1ccacce66657d693af647f7da877eeaab5de4df
k4xxx <mr_foo@163.com>
2022-03-10 11:15:48 +0800

    style: remove unused code and file

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-git pull origin $version
-
-# docker build --build-arg APP_NAME=tone-runner --build-arg ENV=daily -f docker/Dockerfile -t tone-runner:v1.0.0 .
+docker build --build-arg APP_NAME=tone-runner --build-arg ENV=daily -f docker/Dockerfile -t tone-runner:v1.0.0 .
