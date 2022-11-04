15269a000a674f772b9d4938ec1dc05ee821c445
wb-wk01394711 <wb-wk01394711@alibaba-inc.com>
2022-11-04 18:45:10 +0800

    fix: 随机调度是否调用带标签机器各版本逻辑对齐

--- core/server/alibaba/db_operation.py
+++ core/server/alibaba/db_operation.py
@@ -33,7 +33,11 @@ class AliGroupDbServerOperation(CommonDbServerOperation):
 
     @classmethod
     def _get_rand_server(cls, ws_id):
+        tag_server_id_set = cls._get_tag_object_id_set(
+            ServerProvider.ALI_GROUP, RunMode.STANDALONE
+        )
         return TestServer.select().filter(
+            TestServer.id.not_in(tag_server_id_set),
             TestServer.state == ServerState.AVAILABLE,
             TestServer.spec_use == SpecUseType.NO_SPEC_USE,
             TestServer.ws_id == ws_id,
@@ -61,6 +65,10 @@ class AliGroupDbServerOperation(CommonDbServerOperation):
 
     @classmethod
     def _get_rand_cluster(cls, ws_id):
+        tag_cluster_id_set = cls._get_tag_object_id_set(
+            ServerProvider.ALI_GROUP,
+            RunMode.CLUSTER
+        )
         return TestCluster.select().join(
             TestClusterServer, on=(TestCluster.id == TestClusterServer.cluster_id)
         ).join(
@@ -69,6 +77,7 @@ class AliGroupDbServerOperation(CommonDbServerOperation):
             TestServer.state == ServerState.AVAILABLE,
             TestCluster.is_deleted == DbField.FALSE,
             TestServer.is_deleted == DbField.FALSE,
+            TestCluster.id.not_in(tag_cluster_id_set),
             TestCluster.cluster_type == ServerProvider.ALI_GROUP,
             TestCluster.is_occpuied == DbField.FALSE,
             TestCluster.ws_id == ws_id,
